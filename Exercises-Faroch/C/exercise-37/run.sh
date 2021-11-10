74916eac4d17ec685cc7c8cfb1efb2eb58f5ee81
BorisBug <69357420+BorisBug@users.noreply.github.com>
2021-11-10 10:44:35 +0100

    add shell variables

--- Exercises-Faroch/C/exercise-37/run.sh
+++ Exercises-Faroch/C/exercise-37/run.sh
@@ -8,4 +8,11 @@ gcc exercise-37.c -Wall -o out
 ./out 1 ? 1
 ./out 300 % 0
 ./out 222 / 0
+
+a=10; b=3; op="%"; # op can be "+", "-", "//", "* ", "%"
+./out $a "$op" $b
+
+a=23; b=5; op="*"; # op can be "+", "-", "//", "* ", "%"
+./out $a "$op" $b
+
 rm out
